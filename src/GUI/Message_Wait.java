9c2891037d14444bd14db773384c9449bb4ec36d
Jingw98 <sherry-wang95@hotmail.com>
2015-09-23 15:51:54 +1200

    new

--- src/GUI/Message_Wait.java
+++ src/GUI/Message_Wait.java
@@ -18,23 +18,20 @@ public class Message_Wait extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	//A pop-up window to show the message while generating video file
+	// A pop-up window to show the message while generating video file
 	public Message_Wait() {
-		setBounds(300, 70, 450, 150);
-		
+		setBounds(300, 300, 300, 100);
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
-		
-		
-		JLabel lblNewLabel = new JLabel("Generating video file"
-				+ "Please wait");
+
+		JLabel lblNewLabel = new JLabel("<html>Generating video file"
+				+ "<p>   Please wait</html>");
 		lblNewLabel.setBounds(0, 11, 284, 62);
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		contentPane.add(lblNewLabel);
-		
-		
+
 		setContentPane(contentPane);
 		setVisible(true);
 	}
