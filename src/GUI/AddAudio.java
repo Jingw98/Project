7660ed6e85586e095c33ecfb9bdd0bbfa131f759
JingW <sherry-wang95@hotmail.com>
2015-09-24 14:48:19 +1200

    Update AddAudio.java

--- src/GUI/AddAudio.java
+++ src/GUI/AddAudio.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
 
@@ -50,11 +52,19 @@ public class AddAudio extends JFrame {
 		JButton btnOverlay = new JButton("Overlay");
 		JButton btnOverlap = new JButton("Overlap");
 		JButton cancel = new JButton("Cancel");
+		
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				Player.activeMenubar();
+				dispose();
+			}
+		});
 
 		// Cancel button, close the window
 		cancel.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				Player.activeMenubar();
 				dispose();
 			}
 		});
